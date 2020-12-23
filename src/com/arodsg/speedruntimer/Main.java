8bf5e427673a2b878b085276a05784de069ec163
andrewpr52 <andrewpr52@gmail.com>
2020-12-23 13:46:42 -0600

    Updated files for SpeedrunTimer project

--- src/com/arodsg/speedruntimer/Main.java
+++ src/com/arodsg/speedruntimer/Main.java
@@ -1,63 +1,26 @@
-package com.arodsg.spawnsinglemob;
+package com.arodsg.speedruntimer;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Monster;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener, TabCompleter {
 	
-	Map<String, EntityType> mobMap = new HashMap<String, EntityType>();
-	{
-		mobMap.put("blaze", EntityType.BLAZE);
-		mobMap.put("creeper", EntityType.CREEPER);
-		mobMap.put("drowned", EntityType.DROWNED);
-		mobMap.put("elderguardian", EntityType.ELDER_GUARDIAN);
-		mobMap.put("enderdragon", EntityType.ENDER_DRAGON);
-		mobMap.put("evoker", EntityType.EVOKER);
-		mobMap.put("ghast", EntityType.GHAST);
-		mobMap.put("guardian", EntityType.GUARDIAN);
-		mobMap.put("hoglin", EntityType.HOGLIN);
-		mobMap.put("husk", EntityType.HUSK);
-		mobMap.put("magmacube", EntityType.MAGMA_CUBE);
-		mobMap.put("phantom", EntityType.PHANTOM);
-		mobMap.put("piglin", EntityType.PIGLIN);
-		mobMap.put("piglinbrute", EntityType.PIGLIN_BRUTE);
-		mobMap.put("pillager", EntityType.PILLAGER);
-		mobMap.put("ravager", EntityType.RAVAGER);
-		mobMap.put("shulker", EntityType.SHULKER);
-		mobMap.put("silverfish", EntityType.SILVERFISH);
-		mobMap.put("skeleton", EntityType.SKELETON);
-		mobMap.put("skeletonhorse", EntityType.SKELETON_HORSE);
-		mobMap.put("slime", EntityType.SLIME);
-		mobMap.put("spider", EntityType.SPIDER);
-		mobMap.put("stray", EntityType.STRAY);
-		mobMap.put("vindicator", EntityType.VINDICATOR);
-		mobMap.put("witch", EntityType.WITCH);
-		mobMap.put("wither", EntityType.WITHER);
-		mobMap.put("witherskeleton", EntityType.WITHER_SKELETON);
-		mobMap.put("zoglin", EntityType.ZOGLIN);
-		mobMap.put("zombie", EntityType.ZOMBIE);
-		mobMap.put("zombievillager", EntityType.ZOMBIE_VILLAGER);
-	}
+	Boolean isRunning = false;
+	Timer timer;
 	
-	EntityType selectedEntityType;
-	Boolean isEnabled = false;
+	long lastStartTimeMS = 0;
+	long totalDurationMS = 0;
+	
+	long runningSeconds = 0;
 	
     @Override
     public void onEnable() {
@@ -70,24 +33,6 @@ public class Main extends JavaPlugin implements Listener, TabCompleter {
     	
     }
     
-    @EventHandler
-    public void onOverworldMobSpawn(CreatureSpawnEvent event) {
-    	LivingEntity eventEntity = event.getEntity();
-    	
-    	if(isEnabled && eventEntity != null && event.getSpawnReason() == SpawnReason.NATURAL && eventEntity instanceof Monster) { //if enabled, and the entity is not null, and the creature spawned naturally, and the creature is a monster entity
-    		World mobWorld = event.getEntity().getWorld();
-	    	
-	    	if(mobWorld.getEnvironment() == World.Environment.NORMAL) { //if the mob's world is a normal world (not the Nether or End), spawn a Skeleton in its place
-	    		spawnMob(mobWorld, eventEntity.getLocation());
-	    		eventEntity.remove();
-	    	}
-    	}
-    }
-    
-    public void spawnMob(World world, Location location) { //spawns the specified mob in the given world at the given location
-    	world.spawnEntity(location, selectedEntityType);
-    }
-    
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
 		return null;
@@ -97,36 +42,148 @@ public class Main extends JavaPlugin implements Listener, TabCompleter {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     	String commandName = command.getName();
     	
-    	if ((commandName.equalsIgnoreCase("spawnsinglemob") || commandName.equalsIgnoreCase("ssm")) && args.length > 0) {
+    	if ((commandName.equalsIgnoreCase("hardcoretimer") || commandName.equalsIgnoreCase("hct")) && args.length > 0) {
     		if(args.length > 0) {
 	    		switch(args[0]) {
-	    			case "enable":
-	    				if(args.length == 2) {
-	    					String mobName = args[1];
+	    			case "start":
+	    				if(!isRunning) {
+		    				isRunning = true;
+		    				
+		    				timer = new Timer();
+		    			    timer.scheduleAtFixedRate(new TimerTask() {
+	    			            public void run() {
+	    			            	runningSeconds++;
+	    			            	
+	    			            	if(runningSeconds % 900 == 0) { // every 15 minutes
+	    			            		Bukkit.getServer().broadcastMessage("Current duration: " + convertSecondsToString(runningSeconds));
+	    			            	}
+	    			            }
+	    			        }, 1000, 1000);
+		    			    
+		    			    sender.sendMessage("Timer started!");
+		    			    return true;
+	    				}
+	    				else { // if already running
+	    					sender.sendMessage("Error: timer is already running.");
+	    				}
+	    				break;
+	    			case "stop":
+	    				if(isRunning) { // if running, then stop
+	    					isRunning = false;
+	    					timer.cancel();
 	    					
-	    					if(mobMap.containsKey(mobName)) {
-	    						selectedEntityType = mobMap.get(mobName);
-	    						isEnabled = true;
+	    					sender.sendMessage("Timer stopped!");
+	    					return true;
+	    				}
+	    				else { // if not running
+	    					sender.sendMessage("Error: no active timer running.");
+	    				}
+	    				break;
+	    			case "reset":
+	    				resetTimer();
+	    				
+	    				sender.sendMessage("Timer has been reset!");
+	    				return true;
+	    			case "time":
+	    				sender.sendMessage("Current duration: " + convertSecondsToString(runningSeconds));
+	    				return true;
+	    			case "add":
+	    				if(args.length == 3) {
+	    					int numToAdd;
+	    					
+	    					try {
+	    						numToAdd = Integer.parseInt(args[2]);
 	    					}
-	    					else {
-	    						//error, invalid mob name
+	    					catch(NumberFormatException e) {
+	    						sender.sendMessage("Error: invalid argument \"" + args[2] + "\".");
+	    						break;
 	    					}
+	    					
+		    				switch(args[1]) {
+		    					case "hours":
+		    						runningSeconds += (numToAdd * 3600);
+		    						sender.sendMessage("Time set to " + convertSecondsToString(runningSeconds));
+		    						return true;
+		    					case "minutes":
+		    						runningSeconds += (numToAdd * 60);
+		    						sender.sendMessage("Time set to " + convertSecondsToString(runningSeconds));
+		    						return true;
+		    					case "seconds":
+		    						runningSeconds += numToAdd;
+		    						sender.sendMessage("Time set to " + convertSecondsToString(runningSeconds));
+		    						return true;
+		    					default:
+		    						sender.sendMessage("Error: invalid argument.");
+		    						break;
+		    				}
 	    				}
 	    				else {
-	    					//error, invalid number of arguments
+	    					sender.sendMessage("Error: invalid number of arguments.");
 	    				}
+	    				
 	    				break;
-	    			case "disable":
-	    				selectedEntityType = null;
-	    				isEnabled = false;
+	    			case "subtract":
+	    				if(args.length == 3) {
+	    					int numToSubtract;
+	    					
+	    					try {
+	    						numToSubtract = Integer.parseInt(args[2]);
+	    					}
+	    					catch(NumberFormatException e) {
+	    						sender.sendMessage("Error: invalid argument \"" + args[2] + "\".");
+	    						break;
+	    					}
+	    					
+		    				switch(args[1]) {
+		    					case "hours":
+		    						runningSeconds -= (numToSubtract * 3600);
+		    						runningSeconds = runningSeconds >= 0 ? runningSeconds : 0;
+		    						sender.sendMessage("Time set to " + convertSecondsToString(runningSeconds));
+		    						return true;
+		    					case "minutes":
+		    						runningSeconds -= (numToSubtract * 60);
+		    						runningSeconds = runningSeconds >= 0 ? runningSeconds : 0;
+		    						sender.sendMessage("Time set to " + convertSecondsToString(runningSeconds));
+		    						return true;
+		    					case "seconds":
+		    						runningSeconds -= numToSubtract;
+		    						runningSeconds = runningSeconds >= 0 ? runningSeconds : 0;
+		    						sender.sendMessage("Time set to " + convertSecondsToString(runningSeconds));
+		    						return true;
+		    					default:
+		    						sender.sendMessage("Error: invalid argument.");
+		    						break;
+		    				}
+	    				}
+	    				else {
+	    					sender.sendMessage("Error: invalid number of arguments.");
+	    				}
+	    				
 	    				break;
+	    			default:
+	    				sender.sendMessage("Error: invalid argument.");
 	    		}
     		}
     		else {
-    			//error, invalid number of arguments, no arguments provided
+    			sender.sendMessage("Error: no arguments provided.");
     		}
     	}
     	
         return false;
+    }
+    
+    private String convertSecondsToString(long seconds) {
+    	return String.format("%02d:%02d:%02d",
+			TimeUnit.SECONDS.toHours(seconds),
+			TimeUnit.SECONDS.toMinutes(seconds) -
+			TimeUnit.HOURS.toMinutes(TimeUnit.SECONDS.toHours(seconds)),
+			TimeUnit.SECONDS.toSeconds(seconds) -
+			TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(seconds)));
+    }
+    
+    private void resetTimer() {
+    	isRunning = false;
+    	timer.cancel();
+    	runningSeconds = 0;
     }
 }
